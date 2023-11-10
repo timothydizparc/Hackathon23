e1c39140d5b664ba780152826e6e3215cd8c35f3
Timothy Biörck <timothy.biorck@dizparc.se>
2023-11-10 15:16:03 +0100

    style

--- hackathon-ui/pages/index.vue
+++ hackathon-ui/pages/index.vue
@@ -1,51 +1,59 @@
 <template>
   <div
-    class="bg-gray-100 min-h-screen flex flex-col justify-center items-center"
+    class="bg-gradient-to-r from-pink-500 to-orange-500 min-h-screen flex flex-col justify-center items-center"
   >
-    <h1 class="text-4xl font-bold mb-4">FrEAKyVoICe.io</h1>
-    <p class="text-lg">
-      Fill in the form to generate some sick voicelines by your favourite famous
-      person/character
-    </p>
-    <form class="mt-4" @submit.prevent="onSubmit">
-      <label class="block mb-2 font-bold text-gray-700">Voice</label>
-
-      <select
-        v-model="selectedModel"
-        class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-      >
-        <option :value="null" disabled selected>Select a voice</option>
-        <option
-          v-for="(model, index) in voiceModels"
-          :key="index"
-          :value="model"
+    <div class="bg-black rounded-xl p-6 text-white">
+      <div class="text-center">
+        <h1
+          class="text-5xl font-extrabold mb-4 bg-gradient-to-l from-purple-700 to-cyan-400 inline-block text-transparent bg-clip-text"
         >
-          {{ model.title }}
-        </option>
-      </select>
+          FrEAKyVoICe.io
+        </h1>
+      </div>
+      <p class="text-xl">
+        Fill in the form to generate some sick voicelines by your favourite
+        famous person/character
+      </p>
+      <form class="mt-4" @submit.prevent="onSubmit">
+        <label class="block mb-2 font-bold">Voice</label>
 
-      <div v-if="voiceRating">Voice rating: {{ voiceRating }}/10</div>
+        <select
+          v-model="selectedModel"
+          class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+        >
+          <option :value="null" disabled selected>Select a voice</option>
+          <option
+            v-for="(model, index) in voiceModels"
+            :key="index"
+            :value="model"
+          >
+            {{ model.title }}
+          </option>
+        </select>
 
-      <label class="block mb-2 font-bold text-gray-700" for="name">
-        Name
-      </label>
-      <input
-        class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-        id="name"
-        type="text"
-        placeholder="Write some text"
-        v-model="textToVoice"
-      />
-      <button
-        class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
-        type="submit"
-        :disabled="!isValidForm"
-      >
-        Submit
-      </button>
-    </form>
+        <div v-if="voiceRating">Voice rating: {{ voiceRating }}/10</div>
 
-    <div v-if="!isValidForm">Form has errors</div>
+        <label class="block mb-2 font-bold" for="name"> Name </label>
+        <input
+          class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          placeholder="Write some text"
+          v-model="textToVoice"
+        />
+        <button
+          class="px-4 py-2 font-bold text-white bg-cyan-500 rounded hover:bg-pink-500 focus:outline-none focus:shadow-outline-blue cursor-pointer"
+          type="submit"
+          :disabled="!isValidForm"
+        >
+          Submit
+        </button>
+      </form>
+
+      <div v-if="!isValidForm" class="mt-2 font-bold text-red-600">
+        Form has errors
+      </div>
+    </div>
   </div>
 </template>
 
@@ -56,11 +64,6 @@ import type { TTSRequest } from "~/models/TTSRequest";
 const voiceModels = ref<VoiceModel[]>([]);
 const selectedModel = ref<VoiceModel | null>(null);
 const textToVoice = ref<string>("");
-
-onBeforeMount(async () => {
-  const { data } = await useFetch("/api/voices");
-  voiceModels.value = data.value!.voiceModels;
-});
 
 const isValidForm = computed(
   () =>
@@ -89,4 +92,7 @@ function onSubmit(): void {
   };
   alert("Submitted!");
 }
+
+const { data } = await useFetch("/api/voices");
+voiceModels.value = data.value!;
 </script>
--- hackathon-ui/server/api/voices.ts
+++ hackathon-ui/server/api/voices.ts
@@ -1,8 +1,6 @@
 import { VoiceModel } from "~/models/VoiceModel";
 import voiceModels from "../../assets/voice-models.json";
 
-export default defineEventHandler((event): { voiceModels: VoiceModel[] } => {
-  return {
-    voiceModels: voiceModels.models as VoiceModel[],
-  };
+export default defineEventHandler((event): VoiceModel[] => {
+  return voiceModels.models as VoiceModel[];
 });
